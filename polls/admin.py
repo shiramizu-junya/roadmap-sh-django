981bd19c17c78944cd231af4b5b7d5100cba488a
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2026-05-09 15:59:48 +0900

    feat: register Choice and Question models in admin with inline editing

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    date_hierarchy = "pub_date"
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        (
+            "公開情報",
+            {
+                "fields": ["pub_date"],
+                "classes": ["collapse"],
+            },
+        ),
+    ]
+    inlines = [ChoiceInline]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["choice_text", "question", "votes"]
+    list_filter = ["question"]
+    search_fields = ["choice_text"]
